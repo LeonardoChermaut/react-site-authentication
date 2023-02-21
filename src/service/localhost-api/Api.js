023e450240469ad773ba501a3f0193f311307377
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-02-21 16:03:40 -0300

    delete: older directory

--- src/service/localhost-api/Api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const TOKEN = localStorage.getItem("token");
-
-export const LOCALHOST = axios.create({
-  baseURL: "http://localhost:8080",
-});
-LOCALHOST.defaults.headers.common["Authorization"] = `Bearer ${TOKEN}`;
--- src/service/unsplash-api/Api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const TOKEN_UNSPLASH = "WvGGi9k2umEAFhpWIHMopPPXGQ6CxwSlMSihD1PAhLw";
-
-export const unsplashApi = axios.create({
-  baseURL: `https://api.unsplash.com/photos/?client_id=${TOKEN_UNSPLASH}`,
-});
